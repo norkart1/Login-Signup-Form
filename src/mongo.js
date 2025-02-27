cd2ace31309379a14254855f5b3aba916e887851
muhammednavasms <muhammednavasms@gmail.com>
2025-02-27 15:39:08 +0000

    gg

--- src/mongo.js
+++ src/mongo.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/LoginFormPractice")
+mongoose.connect("mongodb+srv://norkart:7nQ6n3jFDudBmQC@navas.ceezf.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{
     console.log('mongoose connected');
 })
